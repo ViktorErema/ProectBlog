4e5ac165474c4fe465890883eab83632052c1f5c
ViktorErema <erema.1996@bk.ru>
2023-09-12 18:06:42 +0300

    СДЕЛАЛ ДОБАВКУ КОММЕНТОВ ОТ ПОЛЬЗОВАТЕЛЕЙ

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from blog.models import Post, Category
-
+from blog.models import Post, Comments
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -10,4 +9,12 @@ class PostForm(forms.ModelForm):
             'text',
             'status_published_post',
 
+        )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = (
+        'text',
         )
